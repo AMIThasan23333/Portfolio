24d35e6640a507cd323e1ce901be2d4d06efff03
My Name <cpgsom819@gmail.com>
2023-05-01 04:32:46 +0800

    adding sidebar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,8 +1,29 @@
 import React from 'react'
-import "./Sidebar.css"
+import "./Sidebar.css";
+
+import Logo from '../assets/logo.svg'
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className='aside'>
+      <a href='#home' className='nav_logo'>
+        <img src={Logo} alt=''/>
+      </a>
+
+      <nav className="nav">
+        <div className="nav-menu">
+            <ul className="nav_list">
+                <li className="nav_item">
+                    <a href=" "  className="nav_link">
+                             
+                    </a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+
+
+
+    </aside>
   )
 }
 
