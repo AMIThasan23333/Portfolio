160876b0ab29a5caab318d529e73a58a8acecbd8
My Name <cpgsom819@gmail.com>
2023-05-01 23:05:36 +0800

    adding portfolio

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Portfolio.css"
+import Menu from './Menu';
+
 const Portfolio = () => {
+
+  const [items, setItems] = useState(Menu)
+
   return (
-    <div>Portfolio</div>
+    <div className="section word container section" id="work">
+
+      <h2 className="section_title">
+                Recent Works 
+      </h2>
+
+      <div className="work_filters">
+        <span className='work_item'>Everything </span>
+        <span className='work_item'>Creative </span>
+
+        <span className='work_item'>Art</span>
+        <span className='work_item'>Design</span>
+        <span className='work_item'>Branding </span>
+
+      </div>
+
+      <div className="work_container grid">
+
+              {
+                items.map((elem) => {
+
+                  const {id,image,title,category} = elem;
+
+
+                  return (
+
+                    <div className="work_card" key={id}>
+                      <div className="work_thumbnail">
+                        <img src={image} alt="" srcset="" />
+                        <div className="work_mask"></div>
+                      </div>
+
+                      <span className='work_category'>{category}</span>
+
+                      <h3 className='work_title'> {title}</h3>
+                      <a href='#' className='work_button'> 
+                      
+                      <i className="icon-link work_button-icon"></i>
+                       </a>
+
+                    </div>
+                  )
+                })
+              }
+
+      </div>
+    </div>
   )
 }
 
