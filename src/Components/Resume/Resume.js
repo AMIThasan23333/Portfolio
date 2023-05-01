e55a6491cb0ba5ecc097a893e85fb521c342f3f2
My Name <cpgsom819@gmail.com>
2023-05-01 22:34:40 +0800

    experience section done

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Resume.css';
+import Data  from './Data';
+import Card from './Card';
 const Resume = () => {
   return (
     <section className='resume container section' id='resume'>
@@ -7,7 +9,39 @@ const Resume = () => {
     <h2 className='section_title'>Experience </h2>
 
     <div className='resume_container grid'>
-      <div className='timeline grid'></div>
+
+      <div className='timeline grid'>
+     {Data.map ((val,id) =>  {
+     
+        if(val.category === 'education'){
+
+          return ( <Card  key={id} icon={val.icon} title={val.title} 
+          year={val.year}  desc={val.desc}
+
+          
+          />)
+        }
+
+     })}
+
+      </div>
+
+
+      <div className='timeline grid'>
+     {Data.map ((val,id) =>  {
+     
+        if(val.category === 'experience'){
+
+          return ( <Card  key={id} icon={val.icon} title={val.title} 
+          year={val.year}  desc={val.desc}
+
+          
+          />)
+        }
+
+     })}
+
+      </div>
     </div>
 
     </section>
