e3c4d6d84e0e5776e2cfc43e2c9fcce970acbe71
My Name <cpgsom819@gmail.com>
2023-05-01 16:49:32 +0800

    adding css in home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,18 +2,20 @@ import React from 'react'
 import "./Home.css"
 import me from '../../Components/assets/avatar-1.svg'
 import HeaderSocial from './HeaderSocial'
+import Scrolldown from './Scrolldown'
 const Home = () => {
   return (
-    <section className='home-container' id='home'>
+    <section className='homes' id='home'>
 
       <div className="intro">
         <img src={me} alt="" srcset="" />
         <h1 className='home_name'>M.G MORSHED </h1>
-        <span className='home_education'>I'm a Front-End Developer   </span>
+        <span className='home_education'>I'm a Front-End Developer</span>
 
         <HeaderSocial></HeaderSocial>
 
         <a href='#contact' className='btn'> Hire Me </a>
+        <Scrolldown></Scrolldown>
       </div>
 
 
